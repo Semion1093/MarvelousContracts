8e1c036c78be109345436e8b1b282aae59829537
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-03-24 18:52:44 +0300

    add TransactionExchangeModel

---
+++ MarvelousContracts/ExchangeModels/TransactionExchangeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Marvelous.Contracts.ExchangeModels
+{
+    public class TransactionExchangeModel
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public int AccountId { get; set; }
+        public Currency Currency { get; set; }
+        public decimal RubRate { get; set; }
+    }
+}
