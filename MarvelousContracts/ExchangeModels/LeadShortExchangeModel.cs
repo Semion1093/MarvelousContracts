df8bf6a172f71d775374bf4d2a83ef55593be668
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-03-31 20:49:34 +0300

    delete LeadChangeRoleRequest, change LeadShortExchangeModel

--- MarvelousContracts/ExchangeModels/LeadShortExchangeModel.cs
+++ MarvelousContracts/ExchangeModels/LeadShortExchangeModel.cs
@@ -6,6 +6,7 @@ namespace Marvelous.Contracts.ExchangeModels
     {
         public int Id { get; set; }
         public Role Role { get; set; }
+        public string Email { get; set; }
     }
 }
 
