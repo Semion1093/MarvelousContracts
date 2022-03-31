4812a20e2810e00eff51b4cfe94ba5235d513213
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-03-31 16:13:11 +0300

    add LeadChangeRoleRequest

---
+++ MarvelousContracts/RequestModels/LeadChangeRoleRequest.cs
@@ -0,0 +1,10 @@
+﻿using Marvelous.Contracts.Enums;
+
+namespace Marvelous.Contracts.RequestModels
+{
+    public class LeadChangeRoleRequest
+    {
+        public int Id { get; set; }
+        public Role Role { get; set; }
+    }
+}
