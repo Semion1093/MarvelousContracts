96e4383320f66d25e3a60d54c659444298e1ae42
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-04-01 14:36:48 +0300

    add MicroserviceEnum, add new models for MarvelousConfig

---
+++ MarvelousContracts/Enums/Microservice.cs
@@ -0,0 +1,15 @@
+﻿namespace Marvelous.Contracts.Enums
+{
+    public enum Microservice
+    {
+        MarvelousAccountCheckingByChuZhig = 1,
+        MarvelousReportMicroService,
+        MarvelousConfigs,
+        MarvelousService,
+        TransactionStore,
+        CRM,
+        workerServiceEmail,
+        RatesApi,
+        Auth
+    }
+}
---
+++ MarvelousContracts/RequestModels/ConfigExchangeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Marvelous.Contracts.RequestModels
+{
+    public class ConfigExchangeModel
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
