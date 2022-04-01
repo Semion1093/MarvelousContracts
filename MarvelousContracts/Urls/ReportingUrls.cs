a88bf2ee17880b5920676e05a0da630e4689736b
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-04-01 13:08:17 +0300

    add Urls for Reporting

---
+++ MarvelousContracts/Urls/ReportingUrls.cs
@@ -0,0 +1,9 @@
+﻿namespace Marvelous.Contracts.Urls
+{
+    public class ReportingUrls
+    {
+        public const string Url = "https://piter-education.ru:6010";
+        public const string Auth = "leads-for-auth";
+        public const string ApiLeads = "api/Leads/";
+    }
+}
