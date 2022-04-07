2706457dbc95228084aa3ec69a6944b37322014c
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-04-07 12:26:34 +0300

    add BalanceRequestModel

---
+++ MarvelousContracts/RequestModels/BalanceRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using Marvelous.Contracts.Enums;
+
+namespace Marvelous.Contracts.RequestModels
+{
+    public class BalanceRequestModel
+    {
+        public List<int> AccountIds { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
