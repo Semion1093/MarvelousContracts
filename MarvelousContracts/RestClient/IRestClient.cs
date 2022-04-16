357b5fc4fe6d1c7e64566236162f205ca7cb90da
Semion1093 <93671427+Semion1093@users.noreply.github.com>
2022-04-16 16:25:13 +0300

    add RestClient

---
+++ MarvelousContracts/RestClient/IRestClient.cs
@@ -0,0 +1,13 @@
+﻿using Marvelous.Contracts.Enums;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Auth.BusinessLayer.Helpers;
+
+public interface IRestClient
+{
+    public IAuthenticator? Authenticator { get; set; }
+    public RestClient AddMicroservice(Microservice service);
+    public Task<RestResponse<T>> ExecuteAsync<T>(RestRequest request, CancellationToken cancellationToken = default);
+}
+}
---
+++ MarvelousContracts/RestClient/MarvelousRestClient.cs
@@ -0,0 +1,13 @@
+﻿using Marvelous.Contracts.Enums;
+using RestSharp;
+
+namespace Auth.BusinessLayer.Helpers;
+
+public class MarvelousRestClient : RestClient, IRestClient
+{
+    public RestClient AddMicroservice(Microservice service) =>
+        this.AddDefaultHeader(nameof(Microservice), service.ToString());
+
+    public Task<RestResponse<T>> ExecuteAsync<T>(RestRequest request, CancellationToken cancellationToken = default) =>
+        RestClientExtensions.ExecuteAsync<T>(this, request, cancellationToken);
+}
